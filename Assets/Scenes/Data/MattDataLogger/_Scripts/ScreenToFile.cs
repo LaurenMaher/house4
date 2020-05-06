d2a8961fc11784a1920320c92a866614b72404ad
laurenmaher13@hotmail.com <laurenmaher13@hotmail.com>
2020-05-06 20:54:43 +0100

    test

---
+++ Assets/Scenes/Data/MattDataLogger/_Scripts/ScreenToFile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenToFile : MonoBehaviour
+{
+
+	private Text textInput;
+
+	private void Awake()
+	{
+		textInput = GetComponent<Text>();
+	}
+
+	public void BUTTON_ACTION_LogTextToFile()
+	{
+		AddToLogFile.LogLine(textInput.text);
+	}
+
+	public void BUTTON_ACTION_CreateTextFileWithUserId()
+	{
+		AddToLogFile.userNumber = textInput.text;
+		AddToLogFile.CreateNewLogFileWithId(textInput.text);
+	}
+
+    public void BUTTON_ACTION_StartGame()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("SplashScreen");
+    }
+
+    public void BUTTON_ACTION_MainMenu()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
